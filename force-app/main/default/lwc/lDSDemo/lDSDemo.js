6e51c459536dbb75b0d1bd11165c8b60e5f4440c
Chaitali9890 <chaitali.chaudhari@ceptes.com>
2022-03-23 15:34:52 +0530

    Initial Commit

---
+++ force-app/main/default/lwc/lDSDemo/lDSDemo.js
@@ -0,0 +1,26 @@
+import { LightningElement,api,track } from 'lwc';
+import AccountName from '@salesforce/schema/Account.Name';
+import AccountNumber from '@salesforce/schema/Account.AccountNumber';
+import AccountSite from '@salesforce/schema/Account.Site';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class LDSDemo extends LightningElement {
+    @api recordId;
+    @api objectApiName;
+    @track fields=[AccountName,AccountNumber,AccountSite];
+    connectedCallback()
+    {
+        this.objectApiName=this.objectApiName;
+    }
+   
+    handleSuccess(event)
+    {
+      const evnt=new ShowToastEvent({
+        title:"Success...",
+        message:"Data Saved Successfully",
+        variant: "success",
+        mode:"dismissable"
+      });
+      this.dispatchEvent(evnt);
+    }
+}
